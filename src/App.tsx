43c77eaa99cddcca75e78dd29717291b3fe92e2d
Alexandr1991Sh <an1991sh@yandex.ru>
2023-08-08 08:51:04 +0300

    Хук useResize для отслеживания ширины экрана

--- src/App.tsx
+++ src/App.tsx
@@ -2,22 +2,33 @@ import './App.css';
 import {NavLink, Outlet} from "react-router-dom";
 import styles from "./components/Site.module.css";
 import {S} from "./styles/_appStyles"
+import {useEffect, useState} from "react";
+import {useWindowSize} from "./helpers/useWindowSize";
 
 
 export type FilterValuesType = {}
 
 function App() {
+
+
+    const size = useWindowSize()
+
     return (
         <>
             {/*<Site/>*/}
             <div className={styles.header}><h1>HEADER</h1></div>
             <div className={styles.body}>
-                <div className={styles.nav}>
-                 <S.NavWrapper><NavLink to={'/page/0'}>PAGE 1</NavLink></S.NavWrapper>
-                 <S.NavWrapper><NavLink to={'/page/1'}>PAGE 2</NavLink></S.NavWrapper>
-                 <S.NavWrapper> <NavLink to={'/page/2'}>PAGE 3</NavLink></S.NavWrapper>
-                 <S.NavWrapper> <NavLink to={'/protected'}>PAGE PROTECTED</NavLink></S.NavWrapper>
-                </div>
+
+                {
+                    size>1000
+                        ? <div className={styles.nav}>
+                            <S.NavWrapper><NavLink to={'/page/0'}>PAGE 1</NavLink></S.NavWrapper>
+                            <S.NavWrapper><NavLink to={'/page/1'}>PAGE 2</NavLink></S.NavWrapper>
+                            <S.NavWrapper> <NavLink to={'/page/2'}>PAGE 3</NavLink></S.NavWrapper>
+                            <S.NavWrapper> <NavLink to={'/protected'}>PAGE PROTECTED</NavLink></S.NavWrapper>
+                        </div>
+                        : <div></div>
+                }
 
                 <div className={styles.content}>
                     <Outlet/>
